7ced1d703a348c580404df4c568ead01e958073b
Nightfly <nightfly@email.ua>
2025-01-28 17:10:42 +0200

    basic URLs saving done

--- index.php
+++ index.php
@@ -4,14 +4,42 @@
  * Tiny URL shortener implementation
  */
 class ShortenerService {
-    protected $randLen = 8;
+    /**
+     * Contains default unique identifier lenght
+     *
+     * @var int
+     */
+    protected $randLen = 12; // 4.74*10^18 or 1:4738381338321617 collision probability
+
+    /**
+     * Contains allocated shorten names as id=>idx
+     *
+     * @var array
+     */
     protected $allocList = array();
 
     //some predefined stuff here
     const DATA_PATH = 'data/';
+    const ROUTE_SAVE='shorten';
+    const ROUTE_REDIRECT='go';
 
     public function __construct() {
         $this->loadAlloc();
+    }
+
+    /**
+     * Returns random alpha-numeric string of some lenght
+     * 
+     * @return string
+     */
+    function getRandString() {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+        $string = '';
+        for ($p = 0; $p < $this->randLen; $p++) {
+            $string .= $characters[mt_rand(0, (strlen($characters) - 1))];
+        }
+
+        return ($string);
     }
 
     /**
@@ -25,7 +53,7 @@ class ShortenerService {
      * @return array
      */
     function rcms_scandir($directory, $exp = '', $type = 'all', $do_not_filter = false) {
-        $dir = $ndir = array();
+        $dir = array();
         if (!empty($exp)) {
             $exp = '/^' . str_replace('*', '(.*)', str_replace('.', '\\.', $exp)) . '$/';
         }
@@ -44,14 +72,66 @@ class ShortenerService {
             closedir($fh);
             natsort($dir);
         }
-        return $dir;
+        return ($dir);
     }
 
+    /**
+     * Preloads allocated shorten names
+     *
+     * @return void
+     */
     protected function loadAlloc() {
         $all = $this->rcms_scandir(self::DATA_PATH);
+        if (!empty($all)) {
+            $this->allocList = array_flip($all);
+        }
+    }
 
-        print_r($all);
+    /**
+     * Returns next free url ID
+     *
+     * @return void
+     */
+    public function getFreeId() {
+        $result = '';
+        $result = $this->getRandString();
+        while (isset($this->allocList[$result])) {
+            $result = $this->getRandString();
+        }
+        return ($result);
+    }
+
+    public function saveUrl($url) {
+        $result = '';
+        if (!empty($url)) {
+            if (mb_strlen($url, 'UTF-8') < 255) {
+                $newId = $this->getFreeId();
+                // ORLY? Oo 
+                if (!isset($this->allocList[$newId])) {
+                    $url = trim($url);
+                    $url = strip_tags($url);
+                    if (is_writable(self::DATA_PATH)) {
+                        file_put_contents(self::DATA_PATH . $newId, $url);
+                        $result = $newId;
+                    }
+                }
+            }
+        }
+        return ($result);
     }
 }
 
 $shortener = new ShortenerService();
+
+if (isset($_GET[$shortener::ROUTE_SAVE])) {
+    if (!empty($_GET[$shortener::ROUTE_SAVE])) {
+        print($shortener->saveUrl($_GET[$shortener::ROUTE_SAVE]));
+    }
+}
+
+
+if (isset($_GET[$shortener::ROUTE_REDIRECT])) {
+    if (!empty($_GET[$shortener::ROUTE_REDIRECT])) {
+        //TODO
+    }
+}
